565b734b66bbc413623fe445fd3dd1b8c384ee8b
guosheng <guosheng@lamahui.com>
2015-05-20 13:20:45 +0800

    $.template增加备注
    $.template增加备注

--- src/template.js
+++ src/template.js
@@ -7,6 +7,19 @@ $.extend({
      * @param  {String}         template  [模板字符串]
      * @param  {Array | Object} data      [数据]
      * @return {String}                   [生成后的字符串]
+     * @example
+     *     var tpl = '<li>姓名：<%= name%></li>';
+     *     var str1 = $.template(tpl, {name : 'kyo'});
+     *     console.log(str1);
+     *     
+     *     
+     *     var data2 = [{
+     *         name : 'kyo'
+     *     },{
+     *         name : 'iori'
+     *     }];
+     *     var str2 = $.template(tpl, data2);
+     *     console.log(str2);
      */
     template : function(template, data) {
         if (data && data.length >= 1) {
